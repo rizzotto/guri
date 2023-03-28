8c23e3c349281b8b0bdfcaf3e6c5d18e8edb5181
Guilherme Rizzotto <43299202+rizzotto@users.noreply.github.com>
2023-03-28 16:53:26 -0300

    Update usguri.js

--- src/commands/usguri.js
+++ src/commands/usguri.js
@@ -6,11 +6,16 @@ module.exports = {
 		.setDescription('Playlist dusguri'),
 	execute: async ({client, interaction}) => {
 		const songs = [
+			'https://www.youtube.com/watch?v=gddPOTsX4i0&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA',
+			'https://www.youtube.com/watch?v=kaDOIFghKkY&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA&index=2',
+			'https://www.youtube.com/watch?v=tXC3Ud5QDPE&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA&index=3',
+			'https://www.youtube.com/watch?v=EhiWWAdDGzA&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA&index=4',
+			'https://www.youtube.com/watch?v=8ovJLd2ZDJk&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA&index=5',
+			'https://www.youtube.com/watch?v=BvgiJPXZIy4&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA&index=6',
+			'https://www.youtube.com/watch?v=zy81fLgyVW0&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA&index=7',
+			'https://www.youtube.com/watch?v=hj_LH_t7cro&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA&index=8',
+			'https://www.youtube.com/watch?v=oOqI2ZPmn7U&list=OLAK5uy_kj3jyOrWssJjOSu63foGi9NwZfvV9ZoiA&index=9',
 			'https://soundcloud.com/ahrystan/wazowski-chapadowski', 
-			'https://www.youtube.com/watch?v=NuunFK7y_Ys',
-			'https://www.youtube.com/watch?v=BbBQP3YZWKE',
-			'https://www.youtube.com/watch?v=F1cDAX1v_cE',
-			'https://www.youtube.com/watch?v=-MvhJODhB4s'
 		]
 
 		const playlist = await client.DisTube.createCustomPlaylist(songs, {
